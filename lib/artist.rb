a3e0687cfe85296253fcac45c2307adc8c7a3615
anupama <anupamagadde@yahoo.com>
2019-12-21 22:47:21 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+require "pry"
+class Artist
+    attr_accessor :name
+    
+    def initialize (name)
+        @name = name
+        @songs = []
+    end
+    def add_song(song)
+        @songs << song
+        song.artist = self
+    end
+    def add_song_by_name(name)
+        song = Song.new(name)
+       # song.artist = self
+        add_song(song)
+    end
+    def self.song_count
+        counter = 0
+        Song.all.select {|song|counter += 1}
+        counter
+    end
+    def songs
+       Song.all.select {|song| song.artist == self}
+     #@songs
+    end
+end
